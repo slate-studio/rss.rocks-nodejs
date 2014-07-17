5b0e0bc5cc5c6119b434464cef6cabe8dd80489c
Alexander Kravets <alex@slatestudio.com>
2014-07-17 12:47:33 +0300

    add firebase for worker; add cron job;

--- worker.js
+++ worker.js
@@ -1,3 +1,12 @@
 // worker.js
+// example of integration with Firebase
 
-console.log('worker is working...')
+var Firebase = require('firebase');
+
+firebaseAppName = 'sizzling-fire-6443'
+var firebaseRef = new Firebase('https://'+firebaseAppName+'.firebaseio.com')
+var subscriptionsRef = firebaseRef.child('subscriptions')
+
+subscriptionsRef.on('child_added', function(snapshot) {
+  console.log(snapshot.val())
+});
